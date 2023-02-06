28306fa15a05430913612ea0178f34c664471ebc
Schleidens <alectineschleidens2016@gmail.com>
2023-02-05 22:32:08 -0500

    add border-radius for product component

--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:flex-[1_0_21%] mx-2 my-2 p-2 bg-gray-100">
+  <div class="md:flex-[1_0_21%] mx-2 my-2 p-2 bg-gray-100 rounded-lg">
     <img class="w-full md:h-64 rounded-lg" :src="image" :alt="title" />
     <div class="text-sm">
       <span class="font-light">{{ title }}</span>
