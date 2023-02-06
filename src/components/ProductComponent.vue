684fa0f1ee30224eb4f1929ec839cb0d44ff52a0
Schleidens <alectineschleidens2016@gmail.com>
2023-02-05 22:38:08 -0500

    add link to single product view in product component

--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="md:flex-[1_0_21%] mx-2 my-2 p-2 bg-gray-100 rounded-lg">
-    <img class="w-full md:h-64 rounded-lg" :src="image" :alt="title" />
+    <a :href="/product/ + link">
+      <img class="w-full md:h-64 rounded-lg" :src="image" :alt="title" />
+    </a>
     <div class="text-sm">
       <span class="font-light">{{ title }}</span>
       <br />
@@ -14,6 +16,7 @@ defineProps({
   image: String,
   price: Number,
   title: String,
+  link: String,
 });
 </script>
 
