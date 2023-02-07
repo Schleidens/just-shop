6c0b2de069bd011109ce134a6eb278df5aaa68ae
Schleidens <alectineschleidens2016@gmail.com>
2023-02-06 22:44:22 -0500

    add the nabar in all pages

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="text-gray-600 body-font mt-12 px-5 md:px-16">
+    <div class="container py-8 flex items-center sm:flex-row flex-col">
+      <p
+        class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
+      >
+        © 2023 JustShop
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import IntroSection from "../components/IntroSection.vue";
 import ProductMain from "../components/ProductMain.vue";
+import TheFooter from "../components/TheFooter.vue";
 </script>
 
 <template>
   <main>
     <IntroSection />
     <ProductMain />
+    <TheFooter />
   </main>
 </template>
