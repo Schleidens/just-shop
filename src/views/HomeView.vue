2d4b0f7a9e7c77aec85ea0f52fcbd40ae50444d3
Schleidens <alectineschleidens2016@gmail.com>
2023-02-06 22:46:59 -0500

    fix navbar typo in last commit by removing it from homeView and render it in app for all pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <script setup>
 import IntroSection from "../components/IntroSection.vue";
 import ProductMain from "../components/ProductMain.vue";
-import TheFooter from "../components/TheFooter.vue";
 </script>
 
 <template>
   <main>
     <IntroSection />
     <ProductMain />
-    <TheFooter />
   </main>
 </template>
